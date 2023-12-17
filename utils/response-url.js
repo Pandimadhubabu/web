0d6059f615fa85a6f8b817de01e2d5ce34f90c06
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2023-12-17 20:59:25 +0530

    Update response-url.js

--- utils/response-url.js
+++ utils/response-url.js
@@ -4,7 +4,7 @@ function getResponseUrl(response) {
   const url = new URL(response.url);
 
   // Reset the response URL if the proxy was used.
-  if (url.host === 'chickar.ee') {
+  if (url.host === 'cdn-web.757live.workers.dev') {
     const parts = url.pathname.split('/');
     const domain = parts[2];
     const path = parts[3] ? `/${decode(parts[3])}` : '/';
