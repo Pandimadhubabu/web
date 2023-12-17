643bd3ea7de7bd995e3d5a8bacf7df5738feb3f6
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2023-12-17 21:00:24 +0530

    Update hash-uri.js

--- utils/hash-uri.js
+++ utils/hash-uri.js
@@ -1,7 +1,7 @@
 import md5 from 'md5';
 
 function hashUri(data) {
-  return `https://chickar.ee/object/${md5(JSON.stringify(data))}`;
+  return `https://cdn-web.757live.workers.dev/object/${md5(JSON.stringify(data))}`;
 }
 
 export default hashUri;
